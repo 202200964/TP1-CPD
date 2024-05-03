9912f87aa07d977568eefb95a3818580914ec916
isidoroforo <202200147@estudantes.ips.pt>
2024-05-03 22:27:05 +0100

    final

--- TP1/main.py
+++ TP1/main.py
@@ -38,14 +38,10 @@ def worker(max_prime, lock):
             start_number = max_prime.value * random.randint(2, 8)  # fator aleatorio
         current_number = start_number
         while True:
-            #with lock:
-             #   if current_number < max_prime.value:
-              #      break
             if is_prime(current_number):
                 with lock:
                     if current_number > max_prime.value:
                         max_prime.value = current_number
-                        print("New max prime found by process", current_number, " - pid: ", os.getpid())
                 break
             current_number += 1
 
@@ -73,10 +69,10 @@ def find_max_prime(timeout, num_processes):
 
 
 if __name__ == '__main__':
-    num_processes = int(input("Enter the number of processes to create: "))
-    timeout = int(input("Enter the duration (in seconds) for the program to run: "))
+    num_processes = int(input("Número de processos a criar: "))
+    timeout = int(input("Duração do programa: "))
     max_prime, runtime = find_max_prime(timeout, num_processes)
     max_prime_str = str(max_prime)
     num_digits = len(max_prime_str)
-    print("Maximum prime found:", max_prime, " (", num_digits, ")")
-    print("Runtime:", runtime, "seconds")
+    print("Maior número primo encontrado:", max_prime, " (", num_digits, ")")
+    print("Runtime:", runtime, "segundos")
